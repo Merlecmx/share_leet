ee677095a01eb7e50c7c0432b1c86eddd8bfdc30
xiaotiaopipi <46887620+xiaotiaopipi@users.noreply.github.com>
2019-01-25 17:55:46 +0800

    Update 922.c

--- 922.c
+++ 922.c
@@ -1,15 +1,15 @@
 int* sortArrayByParityII(int* A, int ASize, int* returnSize) {
    *returnSize = ASize;
-   int  *returnarray = malloc(ASize * sizeof(int));
+   int  *returnArray = malloc(ASize * sizeof(int));
     int a = 0,b = 1;
     for(int i = 0;i<ASize;i++){
         if(A[i]%2==0){
-            returnarray[a] = A[i];
+            returnArray[a] = A[i];
             a += 2;
         }else{
-            returnarray[b] = A[i];
+            returnArray[b] = A[i];
             b += 2;
         }
     }
-    return returnarray;
+    return returnArray;
 }
