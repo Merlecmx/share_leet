ec76037377b19127e344176fcad89e4af3186f9a
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2019-01-25 18:20:20 +0800

    array-Array

--- 118.c
+++ 118.c
@@ -1,17 +1,17 @@
 int** generate(int numRows, int** columnSizes)
 {
     int i,j;
-    int** array = (int**)malloc(sizeof(int*)*numRows);
+    int** Array = (int**)malloc(sizeof(int*)*numRows);
      *columnSizes =(int*)malloc(numRows*sizeof(int*));
     for(i=0; i<numRows; i++)
     {
         (*columnSizes)[i] = i + 1;
-        array[i] = (int *)malloc(sizeof(int*)*(*columnSizes)[i]);
-        array[i][0] = 1;
+        Array[i] = (int *)malloc(sizeof(int*)*(*columnSizes)[i]);
+        Array[i][0] = 1;
         for(j=1; j<i+1; j++)
        {
-          array[i][j] = array[i-1][j-1] + array[i-1][j];
+          Array[i][j] = Array[i-1][j-1] + Array[i-1][j];
        }
     }
-    return array;
+    return Array;
 }
