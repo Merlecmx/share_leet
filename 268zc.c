90058f784a939d3b1cfcb75683bdeb8b47b6b1e5
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2019-02-02 22:59:36 +0800

    sum-numSize

--- 268zc.c
+++ 268zc.c
@@ -1,11 +1,10 @@
 
 int missingNumber(int* nums, int numsSize)
 {
-    int sum = numsSize;
     int numssum = 0;
     for(int i=0; i<numsSize; i++)
     {
-        sum += i;
+        numsSize += i;
         numssum += nums[i];
     }
     return sum - numssum;
