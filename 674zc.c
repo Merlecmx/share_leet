973accb59381482c3d8fa4ab15381b64a0183ab6
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2019-02-13 18:27:22 +0800

    lc5

---
+++ 674zc.c
@@ -0,0 +1,29 @@
+int findLengthOfLCIS(int* nums, int numsSize)
+{
+    int count,temp;
+    count = 1;
+    temp = 0;
+    if(numsSize == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        for(int i=1; i<numsSize; i++)
+        {
+            if(nums[i] > nums[i-1])
+            {
+                count++;
+            }
+            else
+            {
+                if(count > temp)
+                {
+                    temp = count;
+                }
+                count = 1;
+            }
+        }
+        return count > temp ? count : temp;
+    }
+}
