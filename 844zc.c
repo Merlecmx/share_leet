44fc25b32acd159f5b06dcec11135637d05436c4
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2019-03-05 16:08:45 +0800

    增加判断malloc

--- 844zc.c
+++ 844zc.c
@@ -15,7 +15,9 @@ int  stack(char *arry,char* arry2) {
 }
 bool backspaceCompare(char* S, char* T) {
     char *arry_s = (char *)malloc(201*sizeof(char));
+    if (arry_s == NULL) return 0;
     char *arry_t = (char *)malloc(201*sizeof(char));
+    if (arry_t == NULL) return 0;
     int count_s = stack(S,arry_s);
     int count_t = stack(T,arry_t);
     bool is_equal = (count_s == count_t && strncmp(arry_s, arry_t, count_s) == 0);
