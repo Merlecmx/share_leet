265866bd60ad212ffeb5e45144dd35ed9b62faa9
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2019-03-12 21:40:16 +0800

    lceasy

---
+++ 235zc.c
@@ -0,0 +1,10 @@
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if(root == p || root == q) return root;
+    if(root->val > p->val && root->val < q->val) return root;
+    if(root->val < p->val && root->val > q->val) return root;
+    if(root->val > p->val) {
+        return lowestCommonAncestor(root->left,p,q);
+    } else {
+        return lowestCommonAncestor(root->right, p,q);
+    }
+}
