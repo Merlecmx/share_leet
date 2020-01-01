50bcbddfed4821decb7ca7a613c7849ca80c0c5b
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-01-01 16:23:06 +0800

    优化

--- 557zc.c
+++ 557zc.c
@@ -1,8 +1,8 @@
 void reverseString(char* S, int x, int y);
+void swap(char* a, char* b);
 char * reverseWords(char * s) {
     int i = 0, j = 1;
     int flag = 0;
-    char temp = 0;
     while(j < strlen(s)) {
         while(s[j] != '\0') {
             if(s[j] == ' ')
@@ -13,11 +13,9 @@ char * reverseWords(char * s) {
         reverseString(s, i, j-1);
         i = flag;
         j = flag + 1;
-        flag = 0;
     }
     return s;
 }
-void swap(char* a, char* b);
 void reverseString(char* S, int x, int y) {
     while(x < y) {
         swap(&S[x], &S[y]);
@@ -26,8 +24,7 @@ void reverseString(char* S, int x, int y) {
     }
 }
 void swap(char* a, char* b) {
-    char temp1;
-    temp1 = *a;
+    char temp1 = *a;
     *a = *b;
     *b = temp1;
 }
