b348a12337fd2346f1009101d3d90bea32fc6e00
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-01-01 14:49:00 +0800

    优化

--- 657zc.c
+++ 657zc.c
@@ -1,19 +1,16 @@
 bool judgeCircle(char * moves) {
     int count1 = 0, count2 = 0;
     int i = 0;
-    while(moves[i] != '\0') {
-        for(i = 0; i < strlen(moves); i++) {
-            if(moves[i] == 'R')
-                count1++;
-            if(moves[i] == 'L')
-                count1--;
-            if(moves[i] == 'U')
-                count2++;
-            if(moves[i] == 'D')
-                count2--;
-        }
+    while(*moves != '\0') {
+        if(*moves == 'R')
+            count1++;
+        if(*moves == 'L')
+            count1--;
+        if(*moves == 'U')
+            count2++;
+        if(*moves == 'D')
+            count2--;
+        moves++;
     }
-    if(count1 == 0 && count2 == 0)
-        return 1;
-    return 0;
+    return !count1 && !count2;
 }
