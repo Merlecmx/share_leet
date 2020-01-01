f04ce4cd5a83505b9cde41d0090445eed5b38bf4
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-01-01 16:14:49 +0800

    添加调用函数

--- 557zc.c
+++ 557zc.c
@@ -1,24 +1,33 @@
+void reverseString(char* S, int x, int y);
 char * reverseWords(char * s) {
     int i = 0, j = 1;
     int flag = 0;
     char temp = 0;
     while(j < strlen(s)) {
         while(s[j] != '\0') {
-            if(s[j] == '\0' || s[j] == ' ')
+            if(s[j] == ' ')
                 break;
             j++;
         }
         flag = j + 1;
-        while(i < j-1) {
-            temp = s[i];
-            s[i] = s[j-1];
-            s[j-1] = temp;
-            i++;
-            j--;
-        }
+        reverseString(s, i, j-1);
         i = flag;
         j = flag + 1;
         flag = 0;
     }
     return s;
+}
+void swap(char* a, char* b);
+void reverseString(char* S, int x, int y) {
+    while(x < y) {
+        swap(&S[x], &S[y]);
+        x++;
+        y--;
+    }
+}
+void swap(char* a, char* b) {
+    char temp1;
+    temp1 = *a;
+    *a = *b;
+    *b = temp1;
 }
