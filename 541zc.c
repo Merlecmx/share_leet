7c4e2c35e3131c45618309c722b66db6d39b64f1
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-01-01 16:28:50 +0800

    增加调用函数

--- 541zc.c
+++ 541zc.c
@@ -1,21 +1,28 @@
+void reverseString(char* S, int x, int y);
+void swap(char* a, char* b);
 char * reverseStr(char * s, int k) {
     int len = strlen(s);
-    int i = 0, j, num = 1;
+    int i = 0, j = 0, num = 1;
     while(i < len) {
         if(len-i < k)
             j = len - 1;
         else
             j = i + k - 1;
-        while(i < j) {
-            char temp;
-            temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i++;
-            j--;
-        }
+        reverseString(s, i, j);
         i = 2*k*num;
         num++;
     }
     return s;
+}
+void reverseString(char* S, int x, int y) {
+    while(x < y) {
+        swap(&S[x], &S[y]);
+        x++;
+        y--;
+    }
+}
+void swap(char* a, char* b) {
+    char temp1 = *a;
+    *a = *b;
+    *b = temp1;
 }
