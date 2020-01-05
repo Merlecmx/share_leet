9129411a21cd460928d1cbcbb0176a3a02e16e85
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-01-05 13:01:36 +0800

    mUTF8

--- 1089zcarray.c
+++ 1089zcarray.c
@@ -1,8 +1,8 @@
 void duplicateZeros(int* arr, int arrSize) {
     for (int i = 0; i < arrSize; i++) {
-        if (arr[i] == 0) {
+        if (arr[i] ==0) {
             int j = arrSize - 1;
-            for (j; j > i + 1; j--) {//0ºóÃæµÄÊıºóÒÆÒ»Î»
+            for (j; j > i + 1; j--) {//0åé¢çš„æ•°åç§»ä¸€ä½
                 arr[j] = arr[j - 1];
             }
             arr[j] = 0;
