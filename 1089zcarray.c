6f6a6cf4d089843c6fb436b02c676d7e24e68a07
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-01-05 18:14:55 +0800

    降低时间复杂度

--- 1089zcarray.c
+++ 1089zcarray.c
@@ -1,12 +1,25 @@
 void duplicateZeros(int* arr, int arrSize) {
-    for (int i = 0; i < arrSize; i++) {
+    int i = 0;
+    int count = 0;
+    for (i; i < arrSize; i++) {
+        count++;
         if (arr[i] == 0) {
-            int j = arrSize - 1;
-            for (j; j > i + 1; j--) {//0后面的数后移一位
-                arr[j] = arr[j - 1];
-            }
-            arr[j] = 0;
-            i++;
+            count++;
         }
+        if (count >= arrSize) {//加入0后需要的长度
+            break;
+        }
+    }
+    for (int j = arrSize - 1; j >= 0; j--) {
+        if (arr[i] != 0) {
+            arr[j] = arr[i];
+        } else if (arr[i] == 0 && count > arrSize && j == arrSize - 1) {//当最后一个是0时，不需要再复制0
+            arr[j] = arr[i];
+        } else {
+            arr[j] = arr[i];//0不在最后一个
+            arr[j - 1] = arr[i];
+            j--;
+        }
+        i--;
     }
 }
