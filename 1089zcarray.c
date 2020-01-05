9fe3dd8fc8f1c1c57b3c68d6b6fbe4bcedc359f0
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-01-05 13:10:19 +0800

    代码规范

--- 1089zcarray.c
+++ 1089zcarray.c
@@ -1,6 +1,6 @@
 void duplicateZeros(int* arr, int arrSize) {
     for (int i = 0; i < arrSize; i++) {
-        if (arr[i] ==0) {
+        if (arr[i] == 0) {
             int j = arrSize - 1;
             for (j; j > i + 1; j--) {//0后面的数后移一位
                 arr[j] = arr[j - 1];
