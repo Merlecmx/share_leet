579f05f9859537ee86f56713e9c7920e111ecb8e
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-01-31 15:18:57 +0800

    add new 3 files

---
+++ 349zchash.c
@@ -0,0 +1,23 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
+    int *res = (int*)malloc(sizeof(int));
+    int hash[1000] = {0};
+    for (int i = 0; i < nums1Size; i++){
+        hash[nums1[i]] = 1;
+    }
+    int length = 0;
+    for (int i = 0; i < nums2Size; i++) {
+        if (hash[nums2[i]] == 1) {
+            res = (int*)realloc(res, ++length * sizeof(int));
+            res[length - 1] = nums2[i];
+            hash[nums2[i]] = 0;
+        }
+    }
+    *returnSize = length;
+    return res;
+}
+
