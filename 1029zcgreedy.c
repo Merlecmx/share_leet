24f3fd29b80448ad282cc67ea6afeda4bd42bd27
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-02-04 19:49:53 +0800

    修改代码

--- 1029zcgreedy.c
+++ 1029zcgreedy.c
@@ -7,40 +7,13 @@ int twoCitySchedCost(int** costs, int costsSize, int* costsColSize){
     int index = 0, sum = 0, flagA = 0, flagB = 0;
     for (int i = 0; i < costsSize; ++i) {
         arr[index++] = costs[i][0] - costs[i][1];
+        sum += costs[i][1];
     }
     qsort(arr, index, sizeof(int), Cmp);
-    int midindex = costsSize / 2;
-    int tempA = arr[midindex - 1];
-    int tempB = arr[midindex];
-    for (int i = 0; i < costsSize; ++i) {
-        int temp = costs[i][0] - costs[i][1];
-        if (temp < tempA) {
-            sum += costs[i][0];
-            flagA++;
-        } else if (temp > tempB) {
-            sum += costs[i][1];
-            flagB++;
-        }
+    int flag = costsSize / 2;
+    for (int i = 0; i < flag; ++i) {
+        sum += arr[i];
     }
-    for (int i = 0; i < costsSize; ++i) {
-        int temp = costs[i][0] - costs[i][1];
-        if (temp == tempA) {
-            if (flagA < midindex) {//在当前值与中间值相等时，判断去A的人是否已经够了
-                sum += costs[i][0];
-                flagA++;
-            } else {
-                sum += costs[i][1];
-                flagB++;
-            }
-        } else if (temp == tempB) {
-            if (flagB < midindex) {
-                sum += costs[i][1];
-                flagB++;
-            } else {
-                sum += costs[i][0];
-                flagA++;
-            }
-        }
-    }
+    free(arr);
     return sum;
 }
