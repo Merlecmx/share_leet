475fd688918080bcc615a7f82291ff42021fb03c
2194028008@qq.com <46887620+xiaotiaopipi@users.noreply.github.com>
2020-02-21 23:04:02 +0800

    修改72

--- 72zcdp.c
+++ 72zcdp.c
@@ -5,24 +5,24 @@ int FindMin(int a, int b){
 int minDistance(char * word1, char * word2){
     int len_word1 = strlen(word1);
     int len_word2 = strlen(word2);
-    int d[len_word1 + 1][len_word2 + 1];
+    int** dp = (int**)malloc(sizeof(int*) * (len_word1 + 1));
+    for (int i = 0; i < len_word1 + 1; ++i) {
+        dp[i] = (int*)calloc(sizeof(int) , (len_word2 + 1));
+    }
     for (int i = 0; i <= len_word1; ++i) {
-        d[i][0] = i;
+        dp[i][0] = i;
     }
     for (int j = 0; j <= len_word2; ++j) {
-        d[0][j] = j;
+        dp[0][j] = j;
     }
     for (int i = 1; i <= len_word1; ++i) {
         for (int j = 1; j <= len_word2; ++j) {
             if (word1[i - 1] == word2[j - 1]) {
-                d[i][j] = d[i - 1][j - 1];
+                dp[i][j] = dp[i - 1][j - 1];
             } else {
-                int leftup = d[i - 1][j - 1];
-                int left = d[i][j - 1];
-                int up = d[i - 1][j];
-                d[i][j] = FindMin(leftup, FindMin(left, up)) + 1;
+                dp[i][j] = FindMin(dp[i - 1][j - 1], FindMin(dp[i][j - 1], dp[i - 1][j])) + 1;
             }
         }
     }
-    return d[len_word1][len_word2];
+    return dp[len_word1][len_word2];
 }
